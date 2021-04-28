18e80b907c78e65cc3a200a9a1c6fac2e84bffdb
Chinchien <LIN810116@gmail.com>
2021-04-28 16:43:23 +1200

    Add example dicom file
    This dicom file is dicomlibrary.com (a free online dicom library for educational and scientific purposes) - https://www.dicomlibrary.com/?manage=feb6447a72c9a0a31e1bb4459e547964

--- examples/create_and_run_project.py
+++ examples/create_and_run_project.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     P = wm.Project(project_name)
     script = P.script('pretend_import')
     script_input_arguments = {'path': 'data/pretend_data.txt'}
+    # script_input_arguments = {'path': 'data/example_dicom.dcm'}
     script.run(script_input_arguments)
 
     wm.project.start_process_monitor(project_name, minutes_alive=1, sleep_time=3, total_cores=8)
