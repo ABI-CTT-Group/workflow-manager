f95033d6f979c68c957c521572bc1834dbb942d4
Chinchien <LIN810116@gmail.com>
2021-04-30 09:44:36 +1200

    Fix bug in docker implementation when project already exists

--- docker/scripts/create_project.py
+++ docker/scripts/create_project.py
@@ -1,7 +1,7 @@
 import sys
 import pymongo
 
-import workflow_manager as project
+import workflow_manager
 
 if __name__ == '__main__':
     if len(sys.argv) < 0:
@@ -10,13 +10,14 @@ if __name__ == '__main__':
     project_root = sys.argv[2]
     script_dir = sys.argv[3]
 
-    myclient = pymongo.MongoClient('localhost', 27017)
-    if not project_name in myclient.list_database_names():
-        P = project.create_project(project_name, root_dir=project_root)
+    conn = pymongo.MongoClient('localhost', 27017)
+    if not project_name in conn.list_database_names():
+        print(conn.list_database_names())
+        P = workflow_manager.create_project(project_name, root_dir=project_root)
 
-    P.import_script(script_dir + '/pretend_import.py')
-    P.import_script(script_dir + '/pretend_segment.py')
-    P.import_script(script_dir + '/pretend_fit.py')
-    P.import_script(script_dir + '/pretend_mechanics1.py')
-    P.import_script(script_dir + '/pretend_mechanics2.py')
-    P.import_script(script_dir + '/pretend_send.py')
+        P.import_script(script_dir + '/pretend_import.py')
+        P.import_script(script_dir + '/pretend_segment.py')
+        P.import_script(script_dir + '/pretend_fit.py')
+        P.import_script(script_dir + '/pretend_mechanics1.py')
+        P.import_script(script_dir + '/pretend_mechanics2.py')
+        P.import_script(script_dir + '/pretend_send.py')
